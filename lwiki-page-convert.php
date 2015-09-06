fc36bada7376558f8d9b7b534abbecb52c253f8f
Koichi Murase <myoga.murase@gmail.com>
2015-09-06 17:51:26 +0900

    lwiki-page-convert.php: bugfix, $pageid を定義する前に、$pageid を必要とするファイルを require していた。

--- lwiki-page-convert.php
+++ lwiki-page-convert.php
@@ -11,20 +11,10 @@ function lwiki_canonicalize_linebreaks($text){
   return preg_replace('/[ \t　]*(?:\r?\n|\r)/u',"\n",$text);
 }
 
-require_once dirname(__FILE__).'/lib/lib.lwiki.php';
-require_once dirname(__FILE__).'/lib/lib.page-edit.php';
-
 $pageid=@$argv[1];
 if(!$pageid)exit;
+
+require_once dirname(__FILE__).'/lib/lib.lwiki.php';
+require_once dirname(__FILE__).'/lib/lib.page-edit.php';
 \lwiki\edit\page_convert();
-
-// if($argv[1]==NULL)
-//   $fname='php://stdin';
-// else
-//   $fname=$argv[1];
-
-// $content=file_get_contents($fname);
-// $content=lwiki\convert\convert($content);
-// echo $content.PHP_EOL;
-
 ?>
